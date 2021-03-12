18f4ec399f1713d543786401d2a3aba93b5e6359
hunzsig <mzyhaohaoren@qq.com>
2021-03-13 02:25:24 +0800

    feat(UI): *

--- scripts/ui.lua
+++ scripts/ui.lua
@@ -5,6 +5,6 @@ UI = function()
             cj.SaveStr(cg.H_HT_UI, idx, 1, "黄金: " .. hplayer.getGold(enumPlayer))
             cj.SaveStr(cg.H_HT_UI, idx, 2, "木头: " .. hplayer.getLumber(enumPlayer))
         end)
-        --cj.ExecuteFunc("H_DZUI_INTERVAL")
+        cj.ExecuteFunc("H_DZUI_INTERVAL")
     end)
 end

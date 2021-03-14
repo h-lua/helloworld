6b4fc34fbbf1bdd2764b1b6596db1bc7a245cf3b
hunzsig <mzyhaohaoren@qq.com>
2021-03-14 14:14:22 +0800

    feat(UI): *

--- scripts/ui.lua
+++ scripts/ui.lua
@@ -1,9 +1,16 @@
 UI = function()
-    htime.setInterval(1, function(_)
+    htime.setInterval(0.1, function(_)
         hplayer.forEach(function(enumPlayer, idx)
             idx = idx - 1 --vjass玩家从0开始索引，故减1
             cj.SaveStr(cg.H_HT_UI_DATA, idx, cg.FK_TXT_DPS, "DPS: " .. math.numberFormat(hplayer.getDamage(enumPlayer), 4))
             cj.SaveStr(cg.H_HT_UI_DATA, idx, cg.FK_TXT_KILL, "杀敌数: " .. math.integerFormat(hplayer.getKill(enumPlayer)))
+            local selection = hplayer.getSelection(enumPlayer)
+            print_mb("selection", hunit.getName(selection))
+            if (selection ~= nil and his.deleted(selection) == false and his.alive(selection)) then
+                cj.SaveInteger(cg.H_HT_UI_DATA, idx, cg.FK_BG_ATTR, 567)
+            else
+                cj.SaveInteger(cg.H_HT_UI_DATA, idx, cg.FK_BG_ATTR, 0)
+            end
         end)
         cj.ExecuteFunc("H_DZUI_INTERVAL")
     end)

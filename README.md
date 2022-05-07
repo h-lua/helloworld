8d015cd54e495fe61d2447b39bdfbca0528e2a00
hunzsig <mzyhaohaoren@qq.com>
2022-05-07 22:09:52 +0800

    chore(*): 调整测试

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# Hello! 世界!
+# h-lua/helloworld
 
 > 这是一个极其简单的h-lua实践
 > 《步兵大战骑士》
 
 ### 必须使用 h-lua-sdk 进行地图构建与测试
 ```
-git clone https://github.com/hunzsig-warcraft3/h-lua-sdk.git
+git clone https://github.com/h-lua/h-lua.git
 ```
 
 ### 将本项目克隆到 h-lua-sdk 的 projects 里
 ```
-cd ./h-lua-sdk/projects
-git clone https://github.com/hunzsig-warcraft3/h-lua-sdk-helloworld.git
+cd ./h-lua/projects
+git clone https://github.com/h-lua/helloworld.git
 ```
 
 ### 测试游戏！
 ```
-sdk.exe test h-lua-sdk-helloworld
+sdk.exe test helloworld
 ```
--- scripts/setup.lua
+++ scripts/setup.lua
@@ -9,30 +9,8 @@ SETUP = function()
     -- 设定敌军
     henemy.set("怪物", nil, { 2 }, false)
 
-    cj.FogEnable(false)
-    cj.FogMaskEnable(false)
-
-    --- 设定获得黄金木头特效
-    hevent.onPlayerResourceChange(function(evtData)
-        if (evtData.triggerUnit ~= nil and evtData.value > 0) then
-            local u = evtData.triggerUnit
-            local val = math.floor(evtData.value)
-            if (evtData.type == 'gold') then
-                htextTag.model({ msg = "+" .. val .. "金", whichUnit = u, red = 255, green = 215, blue = 0 })
-                hsound.voice2Unit(JassGlobals.gg_snd_ReceiveGold, 100, u)
-            elseif (evtData.type == 'lumber') then
-                htextTag.model({ msg = "+" .. val .. "木", whichUnit = u, red = 34, green = 139, blue = 34 })
-                hsound.voice2Unit(JassGlobals.gg_snd_BundleOfLumber, 100, u)
-            end
-        end
-    end)
-
-    --- 经验获取
-    onExp = function(u)
-        hevent.onExp(u, function(evtData)
-            htextTag.model({ msg = "+" .. evtData.value .. " 经验", whichUnit = u, red = 50, green = 50, blue = 251 })
-        end)
-    end
+    cj.FogEnable(not DEBUGGING)
+    cj.FogMaskEnable(not DEBUGGING)
 
     -- 映射属性
     hattribute.setRelation({
@@ -44,4 +22,69 @@ SETUP = function()
             mana_back = 0.1,
         },
     })
+
+    -- 事件反应
+    hmodelTag.char("0", "hLua\\ttg\\n_0.mdl", 1.9)
+    hmodelTag.char("1", "hLua\\ttg\\n_1.mdl", 1.8)
+    hmodelTag.char("2", "hLua\\ttg\\n_2.mdl", 1.8)
+    hmodelTag.char("3", "hLua\\ttg\\n_3.mdl", 1.8)
+    hmodelTag.char("4", "hLua\\ttg\\n_4.mdl", 1.8)
+    hmodelTag.char("5", "hLua\\ttg\\n_5.mdl", 1.8)
+    hmodelTag.char("6", "hLua\\ttg\\n_6.mdl", 1.8)
+    hmodelTag.char("7", "hLua\\ttg\\n_7.mdl", 1.8)
+    hmodelTag.char("8", "hLua\\ttg\\n_8.mdl", 1.8)
+    hmodelTag.char("9", "hLua\\ttg\\n_9.mdl", 1.8)
+    hmodelTag.char("+", "hLua\\ttg\\s_jia.mdl", 1.7)
+    hmodelTag.char("-", "hLua\\ttg\\s_jian.mdl", 1.7)
+
+    local function _z(u, offset)
+        return hunit.getFlyHeight(u) + hunit.z(u) + 130 + offset
+    end
+
+    ---@param evtData onBeDamageData
+    hevent.reaction(CONST_EVENT.beDamage, function(evtData)
+        hmodelTag.word({
+            str = math.floor(evtData.damage),
+            width = 10,
+            size = 0.3,
+            x = hunit.x(evtData.triggerUnit),
+            y = hunit.y(evtData.triggerUnit),
+            z = _z(evtData.triggerUnit, 0),
+            height = 350,
+            duration = 0.3,
+            scale = { 1.2, 0.5 },
+        })
+    end)
+    ---@param evtData onExpData
+    hevent.reaction(CONST_EVENT.exp, function(evtData)
+        hmodelTag.word({
+            str = "+" .. math.floor(evtData.value),
+            width = 7,
+            size = 0.2,
+            x = hunit.x(evtData.triggerUnit),
+            y = hunit.y(evtData.triggerUnit),
+            z = _z(evtData.triggerUnit, 0),
+            height = 50,
+            duration = 0.2,
+        })
+    end)
+    ---@param evtData onPlayerResourceChangeData
+    hevent.reaction(CONST_EVENT.playerResourceChange, function(evtData)
+        hmodelTag.word({
+            str = "+" .. math.floor(evtData.value),
+            width = 7,
+            size = 0.2,
+            x = hunit.x(evtData.triggerUnit),
+            y = hunit.y(evtData.triggerUnit),
+            z = _z(evtData.triggerUnit, 0),
+            height = 50,
+            duration = 0.2,
+        })
+        if (evtData.type == 'gold') then
+            hsound.voice2Unit(JassGlobals.gg_snd_ReceiveGold, 100, evtData.triggerUnit)
+        elseif (evtData.type == 'lumber') then
+            hsound.voice2Unit(JassGlobals.gg_snd_BundleOfLumber, 100, evtData.triggerUnit)
+        end
+    end)
+
 end
